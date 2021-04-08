32029d2ce6433209ddb13c424dcbf95538221e1d
Zachary Kramer <zckramer83@gmail.com>
2021-04-08 16:11:49 -0400

    hooked up client to backend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import io from 'socket.io-client';
+
+let socket;
+const CONNECTION_PORT = 'localhost:3000/';
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [room, setRoom] =useState('');
+  const [userName, setUserName] =useState('');
+
+  useEffect(()=> {
+  socket = io(CONNECTION_PORT)    
+  }, [CONNECTION_PORT]);
+
+  const connectToRoom = () => {
+    socket.emit('join_room', room);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!loggedIn ? (
+        <div className='logIn'>
+          <div className='inputs'>
+            <input type='text' placeholder='Name...' onChange={(e)=>setUserName(e.target.value)}/>
+            <input type='text' placeholder='Room...' onChange={(e)=>setRoom(e.target.value)} />
+            <button onClick={connectToRoom}>Enter Chat</button>
+          </div>
+        </div>
+      ) : (
+        <h1>You are logged in</h1>
+      )
+      }
     </div>
   );
 }
